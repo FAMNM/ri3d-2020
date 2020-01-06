5208fd625c33ef068c607df9c5597108e3e9f303
tgurlik <tgurlik@umich.edu>
2020-01-06 08:52:34 -0500

    Re-added Shooter Shuffleboard adjustments

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems;
 
+import java.util.Map;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-// import edu.wpi.first.networktables.NetworkTableEntry;
-// import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-// import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-// import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -14,37 +17,39 @@ public class Shooter extends SubsystemBase {
     private WPI_VictorSPX m_flywheel = new WPI_VictorSPX(Constants.kFlyWheel);
 
     // Shuffleboard:
-    // private ShuffleboardTab flywheelSpeedTab;
-    // private NetworkTableEntry flywheelSpeedEntry;
+    private ShuffleboardTab flywheelSpeedTab;
+    private NetworkTableEntry flywheelSpeedEntry;
+    private NetworkTableEntry currentFlywheelSpeedEntry;
 
     // Local Variables:
-    // public double kFlywheelSpeed = 0.5;
+    public double kFlywheelSpeed = 0.5;
 
     /**
      * Creates a new instance of this subsystem
      */
-    // public Shooter() {
-    //     // Add a shooter tab to the Shuffleboard:
-    //     flywheelSpeedTab = Shuffleboard.getTab("Shooter");
-    //     // Add a slider to adjust the speed of the shooter:
-    //     flywheelSpeedEntry = flywheelSpeedTab.add("Speed", 0).withWidget(BuiltInWidgets.kNumberSlider)
-    //             .withProperties(Map.of("min", -1, "max", 1)).getEntry();
-    // }
+    public Shooter() {
+        // Add a shooter tab to the Shuffleboard:
+        flywheelSpeedTab = Shuffleboard.getTab("Shooter");
+        // Add a slider to adjust the speed of the shooter:
+        flywheelSpeedEntry = flywheelSpeedTab.add("Speed", 0).withWidget(BuiltInWidgets.kNumberSlider)
+                .withProperties(Map.of("min", -1, "max", 1)).getEntry();
+        currentFlywheelSpeedEntry = flywheelSpeedTab.add("Flywheel speed",                       kFlywheelSpeed).getEntry();
+    }
 
     /**
      * Spins the flywheel
      */
     public void shoot() {
-        m_flywheel.set(Constants.kFlywheelSpeed);
+        m_flywheel.set(kFlywheelSpeed);
     }
 
     /**
      * Sets the speed of the flywheel
      * @param flywheelSpeed The new speed for the flywheel
      */
-    // public void setflywheelSpeed(double flywheelSpeed) {
-    //     this.kFlywheelSpeed = flywheelSpeed;
-    // }
+    public void setflywheelSpeed(double flywheelSpeed) {
+        this.kFlywheelSpeed = flywheelSpeed;
+    }
 
     /**
      * Stops the flywheel
@@ -56,9 +61,12 @@ public class Shooter extends SubsystemBase {
     /**
      * Runs continuously
      */
-    // @Override
-    // public void periodic() {
-    //     // Update the flywheel speed using the speed slider in Shuffleboard:
-    //     kFlywheelSpeed = flywheelSpeedEntry.getDouble(0.5);
-    // }
+    @Override
+    public void periodic() {
+        // Update the flywheel speed using the speed slider in Shuffleboard:
+        kFlywheelSpeed = flywheelSpeedEntry.getDouble(0.5);
+
+        currentFlywheelSpeedEntry.setDouble(kFlywheelSpeed);
+        
+    }
 }
